678858acb6f5b70552a1ac3c8fc5b664bb93110f
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2021-07-23 08:23:42 +0530

    first commit

---
+++ App_Code/MyWebService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for MyWebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class MyWebService : System.Web.Services.WebService
+{
+    public UserDetails SoapHeader;
+    public MyWebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public string AuthenticationUser()
+    {
+        if (SoapHeader == null)
+            return "please provide username and pass";
+        if (string.IsNullOrEmpty(SoapHeader.UserName) || string.IsNullOrEmpty(SoapHeader.Password))
+            return "please provide UserName and pass";
+        if (!SoapHeader.checkCreds(SoapHeader.UserName, SoapHeader.Password))
+            return "invalid username and pass";
+        //create and store token in cache.
+        string token = Guid.NewGuid().ToString();
+        HttpRuntime.Cache.Add(token, SoapHeader.UserName, null, System.Web.Caching.Cache.NoAbsoluteExpiration,
+            TimeSpan.FromMinutes(30),System.Web.Caching.CacheItemPriority.NotRemovable,null);
+        return token;
+    }
+
+
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public string HelloWorld()
+    {
+        if (SoapHeader == null)
+            return "Please call AuthenticationMethod() first";
+        if(!SoapHeader.checkCreds(SoapHeader))
+            return "please call AuthenticationMethod() first";
+        return "Hello" + HttpRuntime.Cache[SoapHeader.Authtoken];
+    }
+
+}
---
+++ App_Code/UserDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserDetails
+/// </summary>
+public class UserDetails:System.Web.Services.Protocols.SoapHeader
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Authtoken { get; set; }
+    public UserDetails()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public bool checkCreds(string UserName,string Password)
+    {
+        if (UserName == "admin" && Password == "admin")
+            return true;
+        else
+            return false;
+    }
+    public bool checkCreds(UserDetails SoapHeader)
+    {
+        if (SoapHeader == null)
+            return false;
+        //check in cache
+        if(!string.IsNullOrEmpty(SoapHeader.Authtoken))
+            return (HttpRuntime.Cache[SoapHeader.Authtoken] !=null);
+        return false;
+    }
+
+
+}
